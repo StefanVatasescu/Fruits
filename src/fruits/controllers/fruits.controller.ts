721b91111d7c7f2fde5f951f16aeea8653196104
Stefan Vatasescu <stefan.vatasescu@global-technical.com>
2022-07-28 15:16:00 +0300

    WIP

---
+++ src/fruits/controllers/fruits.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Delete, Get, Patch, Post } from "@nestjs/common";
+
+@Controller('fruits')
+export class FruitController{
+    @Get('')
+
+    @GetAll('')
+
+    @Post('')
+
+    @Patch('')
+
+    @Delete('')
+
+}
---
+++ src/fruits/services/fruits.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class FruitsService {
+    constructor(){}
+
+    private fruits: Fruit[] = [];
+    
+
+}
